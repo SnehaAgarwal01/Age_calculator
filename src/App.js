362e926d022e69fafb5a59509be408a21c99dab2
SnehaAgarwal01 <sneha.agarwal2021@vitstudent.ac.in>
2023-10-14 17:16:48 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './App.css';
+import AgeCalculator from "./components/agecal";
+
+function App() {
+  return (
+    <div className="App">
+      <AgeCalculator />
+    </div>
+  );
+}
+
+export default App;
